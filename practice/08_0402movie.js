24b61ab57b9d2039d854e7e99dd7df580f478e4e
unknown <plea9911@gamil.com>
2026-04-02 18:26:09 +0900

    2026/04/02 commit

---
+++ practice/08_0402movie.js
@@ -0,0 +1,6 @@
+let key = `b75c1a60bfa46330543dee2a299406f6`;
+
+const getJson = () => {
+    const data_url =`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${key}&targetDt=${targetDt}`;
+    let response = data_url;
+}
