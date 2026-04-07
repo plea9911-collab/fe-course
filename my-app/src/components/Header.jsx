58fbaa8e5314f84b82ae5d268acca9fd84eb9b65
unknown <plea9911@gamil.com>
2026-04-07 16:30:53 +0900

    2026/04/07 commit

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import img01 from '../assets/images/img01.png';
+import './Header.css';
+
+export default function Header() {
+    return(
+        <header className='header'>
+            <img src={img01} alt="animal" width="150px"/>
+            <h1>Header</h1>
+        </header>
+    )
+}
