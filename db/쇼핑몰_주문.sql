ff71cd371054493630de5e354b5ca0286b58fdf4
unknown <plea9911@gamil.com>
2026-04-29 18:01:34 +0900

    2026/04/29 commit

---
+++ db/쇼핑몰_주문.sql
@@ -0,0 +1,237 @@
+/********************************
+	쇼핑몰 주문 시스템 DB 생성
+********************************/
+-- 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS `shoppy2026`;
+show databases;
+use shoppy2026;
+select database();
+/********************************
+	member - mid - trg_member_mid
+    Order - orid - trg_order_orid
+    OrderItem - ord_id - trg_orderitem_ord_id
+    Product - pid - trg_product_pid
+********************************/
+
+-- member 테이블 생성
+show tables;
+create table member(
+	mid 	char(5)	primary key,
+    name	varchar(5)	not null,
+    email	varchar(30) not null,
+    create_at	datetime not null
+);
+
+
+-- order 테이블 생성
+create table `order`(
+	orid 	char(5)	primary key,
+    mid		char(5)	not null,
+    ordata	date not null,
+    constraint fk_order_mid foreign key(mid)
+		references member(mid)
+        on delete cascade
+        on update cascade
+);
+
+
+-- order_time 테이블 생성
+create table order_item(
+	ord_id 	char(7)	primary key,
+    orid	char(5)	not null,
+    pid	char(5) not null,
+    quantity	int,
+    unit_price	int,
+    constraint fk_order_item_orid foreign key(orid)
+		references `order`(orid)
+		on delete cascade
+        on update cascade
+);
+alter table order_item
+	add constraint fk_product_pid foreign key(pid)
+		references product(pid)
+        on delete cascade
+        on update cascade;
+
+-- product 테이블 생성
+create table product(
+	pid		char(5)	 primary key,
+    name	varchar(20)	not null,
+    price	int
+);
+
+show tables;
+
+/********************************
+	member - mid - trg_member_mid
+    Order - orid - trg_order_orid
+    OrderItem - ord_id - trg_orderitem_ord_id
+    Product - pid - trg_product_pid
+********************************/
+
+-- member trigger
+delimiter $$
+create trigger trg_member_mid
+before insert on member
+for each row
+begin
+declare max_code int;  --  'M0001'
+
+-- 현재 저장된 값 중 가장 큰 값을 가져옴
+SELECT IFNULL(MAX(CAST(right(mid, 4) AS UNSIGNED)), 0)
+INTO max_code
+FROM member; 
+
+-- 'M0001' 형식으로 아이디 생성, LPAD(값, 크기, 채워지는 문자형식) : M0001
+SET NEW.mid = concat('M', LPAD((max_code+1), 4, '0')); 
+
+end $$
+delimiter ;
+
+-- order trigger
+delimiter $$
+create trigger trg_order_orid
+before insert on `order`
+for each row
+begin
+declare max_code int;  --  'OR0001'
+
+-- 현재 저장된 값 중 가장 큰 값을 가져옴
+SELECT IFNULL(MAX(CAST(right(orid, 4) AS UNSIGNED)), 0)
+INTO max_code
+FROM `order`; 
+
+-- 'OR0001' 형식으로 아이디 생성, LPAD(값, 크기, 채워지는 문자형식) : OR0002
+SET NEW.orid = concat('OR', LPAD((max_code+1), 3, '0')); 
+
+end $$
+delimiter ;
+
+-- ordertime trigger
+delimiter $$
+create trigger trg_orderitem_ord_id
+before insert on `order_time`
+for each row
+begin
+declare max_code int;
+
+-- 현재 저장된 값 중 가장 큰 값을 가져옴
+SELECT IFNULL(MAX(CAST(right(ord_id, 4) AS UNSIGNED)), 0)
+INTO max_code
+FROM `order_time`; 
+
+SET NEW.ord_id = concat('ORD', LPAD((max_code+1), 4, '0')); 
+
+end $$
+delimiter ;
+
+-- product trigger
+delimiter $$
+create trigger trg_product_pid
+before insert on `product`
+for each row
+begin
+declare max_code int;  --  'P0001'
+
+-- 현재 저장된 값 중 가장 큰 값을 가져옴
+SELECT IFNULL(MAX(CAST(right(pid, 4) AS UNSIGNED)), 0)
+INTO max_code
+FROM `product`; 
+
+-- 'M0001' 형식으로 아이디 생성, LPAD(값, 크기, 채워지는 문자형식) : P0001
+SET NEW.pid = concat('P', LPAD((max_code+1), 4, '0')); 
+
+end $$
+delimiter ;
+-- --------------------------------------------------------------
+-- --------------------------------------------------------------
+-- member 테이블 데이터 입력
+desc member;
+insert into member(name, email, create_at) 
+	values 
+		('홍길동1', 'hong@a.com', now()),
+		('홍길동2', 'hong@a.com', now()),
+		('홍길동3', 'hong@a.com', now()),
+		('홍길동4', 'hong@a.com', now()),
+		('홍길동5', 'hong@a.com', now());
+select * from member;
+
+-- product 테이블 데이터 입력
+insert into product(name, price) 
+values  ('모니터', 1000),
+		('책상', 20000),
+		('노트북', 12000),
+		('키보드', 2000),
+		('마우스',2500);
+select * from product;
+
+-- order 테이블 데이터 입력
+insert into `order`(mid, odate) 
+values  ('M0001', curdate()),
+		('M0004', curdate()),
+		('M0001', curdate()),
+		('M0004', curdate()),
+		('M0005',curdate());
+select * from `order`;
+
+-- order_item 테이블 데이터 입력
+insert into order_item(orid, pid, quantity, unit_price) 
+values  ('OR002', 'P0003', 10, 2000),
+		('OR001', 'P0003', 12, 12000),
+		('OR003', 'P0001', 15, 20000),
+		('OR002', 'P0004', 20, 15000),
+		('OR005', 'P0001', 35, 2500);
+select * from order_item;
+
+-- 홍길동1 고객의 고객명, 이메일, 가입날짜, 주문날짜를 조회
+-- 주문날짜는 년, 월, 일로만 출력
+desc member;
+select 	m.name, 
+		m.email, 
+        m.created_at, 
+        left(o.odate, 10) as odate 
+from member m, `order` o
+where m.mid = o.mid
+	and m.name = '홍길동1';
+
+
+-- 상품별 주문 건수, 상품명, 주문건수 출력
+select 	p.name, 
+		count(quantity) as count
+from product p left outer join order_item oi
+				on p.pid = oi.pid
+group by p.name;      
+
+
+-- 회원이 주문한 내역과 제품명 조회
+-- 회원명, 가입날짜, 주문날짜, 주문수량, 제품명, 가격
+select 	m.name, 
+		m.created_at, 
+        o.odate, 
+        oi.quantity, 
+        p.name, 
+        p.price
+from member m, `order` o, order_item oi, product p
+where m.mid = o.mid 
+	and o.orid = oi.orid
+    and oi.pid = p.pid;
+
+-- 회원이 주문한 내역과 제품명 조회
+-- 회원명, 가입날짜, 주문날짜, 주문수량, 제품명, 가격
+-- 주문되지 않은 모든 제품 출력 
+select  t1.name, 
+		t1.created_at, 
+        t1.odate, 
+        t1.quantity, 
+        p.name, 
+        p.price
+from (select distinct m.name, 
+			 m.created_at, 
+             o.odate, 
+             oi.quantity, 
+             oi.pid
+		from member m, `order` o, order_item oi
+		where m.mid = o.mid 
+		and o.orid = oi.orid) t1 right outer join product p
+										on t1.pid = p.pid;
+
---
+++ db/학적_수강신청.sql
@@ -0,0 +1,245 @@
+/********************************
+	학적과 수강신청 실습 데이터베이스
+********************************/
+-- 데이터베이스 생성
+-- CREATE DATABASE IF NOT EXISTS `enroll2026`;
+show databases;
+use enroll2026;
+select database();
+
+-- [instructor:강사] 테이블 생성
+show tables;
+create table instructor(
+	instructor_no 		int			auto_increment 		primary key,
+    instructor_name		varchar(5)	not null,
+    age					int,
+    gender				char(1)
+);
+desc instructor;
+
+-- [student : 학생] 테이블 생성
+create table student(
+	student_id		int			auto_increment		primary key,
+    student_name	varchar(5)	not null,
+    address			varchar(50)
+);
+desc student;
+
+-- [subject : 과목] 테이블 생성
+create table subject(
+	subject_no		int			auto_increment 		primary key,
+    subject_name	varchar(30)	not null,
+	class_room		varchar(20),
+    instructor_no	int			not null,
+    constraint fk_subject_instructor_no	foreign key(instructor_no)
+		references instructor(instructor_no)
+        on delete cascade
+        on update cascade
+);
+desc subject;
+select * from information_schema.table_constraints
+	where table_name = 'subject';
+
+-- [class_time : 강의시간] 테이블 생성
+create table class_time(
+	time_id		int			auto_increment		primary key,
+    class_time	varchar(10),
+    subject_no 	int			not null,
+    constraint fk_class_time_subject_no		foreign key(subject_no)
+		references	subject(subject_no)
+			on delete cascade
+            on update cascade
+);
+desc class_time;
+select * from information_schema.table_constraints
+	where table_name = 'class_time';
+    
+
+-- [enrollment : 등록] 테이블 생성
+create table enrollment(
+	student_id		int		not null,
+    subject_no		int		not null,
+    grade			char(1),
+    constraint fk_enrollment_student_id		foreign key(student_id)
+		references	student(student_id)
+			on delete cascade
+            on update cascade,
+	constraint fk_subject_subject_no		foreign key(subject_no)
+		references subject(subject_no)
+			on delete cascade
+            on update cascade
+);
+desc enrollment;
+select * from information_schema.table_constraints
+	where table_name = 'enrollment';
+
+-- [instructor : 강사] 테이블 데이터 입력
+desc instructor;
+select * from instructor;    
+insert into instructor(instructor_name, age, gender)
+			values('스미스', 35, 'M');
+insert into instructor(instructor_name, age, gender)
+			values('김영희', 30, 'F');
+insert into instructor(instructor_name, age, gender)
+			values('Anne', 25, 'F');            
+
+-- [student : 학생] 테이블 데이터 입력
+-- 학생 테이블의 address 컬럼에 default 제약 추가 : '서울시 강남구'
+desc student;
+alter table student
+	modify address	varchar(30)	 default '서울시 강남구';
+
+select * from student;    
+insert into student(student_name) values('정효리');
+insert into student(student_name) values('오감자');
+insert into student(student_name) values('안경태');
+insert into student(student_name) values('정주고');
+insert into student(student_name) values('고소해');
+
+-- [subject : 과목] 테이블 데이터 입력
+desc subject;
+select * from instructor;
+select * from subject;
+insert into subject(subject_name, class_room, instructor_no)
+			value('MySQL', '101호', 1);
+insert into subject(subject_name, class_room, instructor_no)
+			value('HTML', '103호', 2);
+insert into subject(subject_name, class_room, instructor_no)
+			value('Oracle', '201호', 1);
+insert into subject(subject_name, class_room, instructor_no)
+			value('React', '301호', 3);
+insert into subject(subject_name, class_room, instructor_no)
+			value('NodeJS', '303호', 3);
+            
+-- [class_time : 강의시간] 테이블 데이터 입력
+desc class_time;
+select * from subject;
+select * from class_time;
+insert into class_time(class_time, subject_no) values('120분', 1);
+insert into class_time(class_time, subject_no) values('160분', 2);
+insert into class_time(class_time, subject_no) values('200분', 3);
+insert into class_time(class_time, subject_no) values('120분', 4);
+insert into class_time(class_time, subject_no) values('100분', 5);
+
+-- [enrollment : 등록] 테이블 데이터 입력
+desc enrollment;
+select * from student; 
+select * from subject;
+select * from enrollment;
+insert into enrollment(student_id, subject_no, grade)  values(1, 1, 'A');
+insert into enrollment(student_id, subject_no, grade)  values(1, 2, 'B');
+insert into enrollment(student_id, subject_no, grade)  values(2, 1, 'C');
+insert into enrollment(student_id, subject_no, grade)  values(3, 1, 'A');
+insert into enrollment(student_id, subject_no, grade)  values(3, 3, 'A');
+insert into enrollment(student_id, subject_no, grade)  values(4, 2, 'D');
+insert into enrollment(student_id, subject_no, grade)  values(4, 1, 'A');
+insert into enrollment(student_id, subject_no, grade)  values(1, 5, 'C');
+insert into enrollment(student_id, subject_no, grade)  values(2, 3, 'A');
+
+-- A학점을 받은 학생의 정보를 조회
+select  s.student_name as '학생명',
+		s.address as '주소',
+        e.grade as '학점'
+	from student s, enrollment e
+    where s.student_id = e.student_id
+		and e.grade = 'A';
+
+select  s.student_name as '학생명',
+		s.address as '주소',
+        e.grade as '학점'
+	from student s inner join enrollment e
+					on s.student_id = e.student_id
+	where e.grade = 'A';
+
+-- C학점을 받은 학생의 정보와 과목명을 조회
+select  st.student_name,
+		st.address,
+        su.subject_name,
+        su.class_room,
+        e.grade
+	from student st, subject su, enrollment e
+    where st.student_id = e.student_id
+		and su.subject_no = e.subject_no
+        and e.grade = 'C';
+
+select  st.student_name,
+		st.address,
+        su.subject_name,
+        su.class_room,
+        e.grade
+	from student st inner join enrollment e on st.student_id = e.student_id
+					inner join subject su   on su.subject_no = e.subject_no
+	where e.grade = 'C';
+
+-- 100분 강의하는 과목정보와 강사정보를 조회
+select i.instructor_name,
+	   i.age,
+       i.gender,
+       su.subject_name,
+       su.class_room,
+       ct.class_time
+	from instructor i, subjcet su, class_time ct
+    where i.instructor_no = su.instructor_no;
+    
+    
+-- 100분 강의하는 강사정보, 과목명을 조회 => 서브쿼리사용, 과목명(스칼라 서브쿼리)
+select  instrnctor_no,
+		instructor_name,
+		age,
+        gender
+	from instructor
+    where instructor_no = (select instructor_no
+							from subject
+							where subject_no = (select subject_no 
+													from class_time
+													where class_time = '100분'))
+				as subject_name
+                from instructor
+							(select subject_name
+							from subject
+							where subject_no = (select subject_no 
+													from class_time
+													where class_time = '100분'));
+
+
+-- 김영희 강사가 강의하는 모든 과목 조회
+select  su.subject_name,
+		su.class_room
+	from instructor i, subject su
+    where i.instructor_no = su.instructor_no
+		and i.instructor_name = '김영희';
+	
+select  su.subject_name,
+		su.class_room
+	from  subject su
+    where su.instructor_no = (select instructor_no from instructor where instructor_name= '김영희');
+
+
+-- 홍길동 강사가 강의하는 과목과 과목을 수강한 학생정보와 성적을 조회
+select i.instructor_name,
+		su.subject_name,
+        su.class_room,
+        st.student_name,
+        e.grade
+	from instructor i, subject su, student st, enrollment e
+    where i.instructor_no = su.instructor_no
+		and su.subject_no = e.subject_no
+        and e.student_id = st.student_id
+        and i.instructor_name = '홍길동';
+
+
+-- 모든 강사가 강의하는 과목과 성적 조회 (모든 강사 포함)
+select count(*) from instructor;  -- 4
+select count(distinct instructor_no) from subject; -- 3
+select * 
+	from instructor i left outer join	subject su
+					  on i.instructor_no = su.instructor_no;
+                      
+
+
+
+
+
+
+
+
